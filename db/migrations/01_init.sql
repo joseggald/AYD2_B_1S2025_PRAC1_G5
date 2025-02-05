5cd916134873202d2b0c177e132163351a146988
joseggald <josegald49@gmail.com>
2025-02-04 21:44:33 -0600

    hotfix: fix modules delete files

--- db/migrations/01_init.sql
+++ db/migrations/01_init.sql
@@ -1,6 +1,6 @@
  -- db/migrations/01_init.sql
-CREATE TABLE IF NOT EXISTS usuarios (
-    cod_empleado VARCHAR(10) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY,
     usuario VARCHAR(50) UNIQUE NOT NULL,
     nombres VARCHAR(150),
     apellidos VARCHAR(150),
@@ -8,7 +8,6 @@ CREATE TABLE IF NOT EXISTS usuarios (
     contrasena VARCHAR(255),
     rol VARCHAR(20) NOT NULL DEFAULT 'USUARIO',
     activo BOOLEAN DEFAULT true,
-    razon_inactivo VARCHAR(255), -- Razón por la que el usuario fue desactivado/eliminado
     ts_creacion TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     ts_actualizacion TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
