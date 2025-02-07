0b3edc6267713777be48d6263bf5b3bd1cf2c5f8
Aldo Vasquez <aldovasquez2014@gmail.com>
2025-02-06 19:27:43 -0600

    202109754: agregar register y login

--- db/migrations/01_init.sql
+++ db/migrations/01_init.sql
@@ -1,12 +1,21 @@
 -- db/migrations/01_init.sql
 
+-- Tabla de usuarios
+CREATE TABLE IF NOT EXISTS users (
+    id_user SERIAL PRIMARY KEY,
+    nombres VARCHAR(100),
+    apellidos VARCHAR(100),
+    correo_electronico VARCHAR(50),
+    contrasena VARCHAR(500)
+);
+
 -- Tabla de pacientes
 CREATE TABLE IF NOT EXISTS patients (
     id_patient SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     lastName VARCHAR(150) NOT NULL,
     cui INT UNIQUE NOT NULL,
-    phone VARCHAR(20) NOT NULL,  -- Cambié a VARCHAR para los teléfonos
+    phone VARCHAR(20) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     age INT NOT NULL,
     gender VARCHAR(20) NOT NULL,
@@ -15,14 +24,14 @@ CREATE TABLE IF NOT EXISTS patients (
 
 -- Tabla de registros (record)
 CREATE TABLE IF NOT EXISTS record (
-    id_record SERIAL PRIMARY KEY,  -- Cambié a SERIAL para autoincremento
-    id_patient INT NOT NULL,  -- Reemplazado id_user por id_patient
-    FOREIGN KEY (id_patient) REFERENCES patients(id_patient) ON DELETE CASCADE  -- Cambié la referencia a la tabla patients
+    id_record SERIAL PRIMARY KEY,
+    id_patient INT NOT NULL,
+    FOREIGN KEY (id_patient) REFERENCES patients(id_patient) ON DELETE CASCADE
 );
 
 -- Tabla de citas
 CREATE TABLE IF NOT EXISTS citas (
-    id_citas SERIAL PRIMARY KEY,  -- Cambié a SERIAL para autoincremento
+    id_citas SERIAL PRIMARY KEY,
     id_record INT NOT NULL,
     date DATE NOT NULL,
     hour INT NOT NULL,
@@ -31,13 +40,13 @@ CREATE TABLE IF NOT EXISTS citas (
 
 -- Tabla de unidades de medida
 CREATE TABLE IF NOT EXISTS unit_measurement (
-    id_unit SERIAL PRIMARY KEY,  -- Cambié a SERIAL para autoincremento
+    id_unit SERIAL PRIMARY KEY,
     unit VARCHAR(50) NOT NULL
 );
 
 -- Tabla de recetas
 CREATE TABLE IF NOT EXISTS recipe (
-    id_recipe SERIAL PRIMARY KEY,  -- Cambié a SERIAL para autoincremento
+    id_recipe SERIAL PRIMARY KEY,
     id_record INT NOT NULL,
     medicine VARCHAR(100) NOT NULL,
     dose INT NOT NULL,
